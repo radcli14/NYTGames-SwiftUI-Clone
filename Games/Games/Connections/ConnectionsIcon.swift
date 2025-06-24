44e772fba4b18d9ddffa61bda164878736db0ce2
radcli14 <radcli14@gmail.com>
2025-06-24 16:42:59 +0200

    made custom connections icon

--- Games/Games/Connections/ConnectionsIcon.swift
+++ Games/Games/Connections/ConnectionsIcon.swift
@@ -8,15 +8,42 @@
 import SwiftUI
 
 struct ConnectionsIcon: View {
-    private let tileColors: [[Color]] = [
-        [.connectionsViolet, .connectionsViolet, .white, .connectionsViolet],
-        [.connectionsViolet, .connectionsViolet, .connectionsViolet, .white],
-        [.connectionsViolet, .white, .connectionsViolet, .connectionsViolet],
-        [.white, .connectionsViolet, .connectionsViolet, .connectionsViolet]
-    ]
+    private let lineWidth: CGFloat = 4
     
     var body: some View {
-        GridIcon(tileColors: tileColors)
+        VStack(spacing: lineWidth) {
+            tileRow(whiteTileIndex: 2)
+            tileRow(whiteTileIndex: 3)
+            tileRow(whiteTileIndex: 1)
+            tileRow(whiteTileIndex: 0)
+        }
+        .background(.black)
+        .padding(.horizontal, 0.5 * lineWidth)
+        .padding(.vertical, lineWidth)
+        .clipShape(.rect(cornerRadius: 2 * lineWidth))
+        .overlay {
+            RoundedRectangle(cornerRadius: 2 * lineWidth)
+                .stroke(lineWidth: 2 * lineWidth)
+        }
+    }
+    
+    func tileRow(whiteTileIndex: Int) -> some View {
+        GeometryReader { geometry in
+            ZStack {
+                Rectangle()
+                    .stroke(.black, lineWidth: lineWidth)
+                    .fill(.connectionsViolet)
+                whiteTile(at: whiteTileIndex, in: geometry)
+            }
+        }
+    }
+    
+    func whiteTile(at index: Int, in geometry: GeometryProxy) -> some View {
+        Rectangle()
+            .stroke(.black, lineWidth: 2 * lineWidth)
+            .fill(.white)
+            .frame(width: geometry.size.width / 4.75)
+            .offset(x: 0.25 * (Double(index) - 1.5) * geometry.size.width)
     }
 }
 
